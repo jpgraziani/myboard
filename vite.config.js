b8cc6e98082a1a622f7ee05db24cf2e4c844c29c
Jonathan Graziani <41130626+jpgraziani@users.noreply.github.com>
2026-04-22 09:57:29 -0700

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Replace 'myboard' below with whatever you name your GitHub repo
-  base: '/https://github.com/jpgraziani/myboard.git/',
+  base: '/myboard/',
 })
